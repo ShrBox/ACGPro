98183b3ddb5fe8268e36d5d37b1dbd0565d987a0
ShrBox <shrbox@outlook.com>
2020-08-28 22:49:24 +0800

    fix

--- src/main/java/shrbox/github/acg/Main.java
+++ src/main/java/shrbox/github/acg/Main.java
@@ -12,17 +12,18 @@ class Main extends PluginBase {
     public static int count = 0;
 
     public void onEnable() {
-        new Timer("starttimer").schedule(new TimerTask() {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
             @Override
             public void run() {
                 count = 0;
             }
-        }, 10 * 60 * 1000, 10 * 60 * 1000);
+        }, 60 * 1000, 60 * 1000);
         System.setProperty("http.agent", "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; QQBrowser/7.0.3698.400)");
         getEventListener().subscribeAlways(GroupMessageEvent.class, (GroupMessageEvent e) -> {
-            if (e.getMessage().contentToString().toLowerCase().contains("acg")) {
+            if (e.getMessage().contentToString().toLowerCase().equals("acg")) {
                 if (count > 15) {
-                    MessageUtils.newChain(new At(e.getSender())).plus("一分钟内调用次数上限！");
+                    e.getGroup().sendMessage(MessageUtils.newChain(new At(e.getSender())).plus("一分钟内调用次数上限！"));
                     return;
                 }
                 count++;
